7aad72de48bbc83fb9eb2808a929e54cbf4d4373
TheFishyOne123 <81216766+TheFishyOne123@users.noreply.github.com>
2023-11-06 06:51:51 -0700

    First Attempt at Boiler Plate For Sheets API

--- Main.py
+++ Main.py
@@ -1,1 +1,12 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+scope = ['https://www.googleapis.com/auth/spreadsheets']
+creds = ServiceAccountCredentials.from_json_keyfile_name('Credentials.json', scope)
+client = gspread.authorize(creds)
+
+#Spread Sheet Variables
+sheet = client.open('Partnership Tracker')
+worksheet = sheet.get_worksheet(0)
+data = worksheet.get_all_records()
+print(data)
