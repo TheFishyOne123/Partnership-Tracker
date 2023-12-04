6b31f47203778ea4a3ec7bcc58b20520d23cacd3
TheFishyOne123 <81216766+TheFishyOne123@users.noreply.github.com>
2023-12-03 21:03:08 -0700

    Update searchbar.jsx

--- Frontend/src/assets/Components/searchbar.jsx
+++ Frontend/src/assets/Components/searchbar.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
 
+const [search,setSearch] = useState("")
 
+const handleChange = (e) => {
+  const { value } = e.target;
+  setSearch({...search, value});
+};
 
 function Searchbar() {
   return (
-      <form className='flex justify-center ml-auto w-6/12 text-black'>
-        <input type="text" className='w-8/12 rounded-2xl h-8 text-center text-lg'></input>
-      </form>
+      <div className='flex justify-center ml-auto w-6/12 text-black' >
+        <input type="text" className='w-8/12 rounded-2xl h-8 text-center text-lg' value={formData.username} onChange={handleChange}></input>
+      </div>
   )
 }
 
