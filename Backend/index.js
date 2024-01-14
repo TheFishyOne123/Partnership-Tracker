40dfc9fc9e00a76f26e0be5f5c683ec9aba135f1
TheFishyOne123 <81216766+TheFishyOne123@users.noreply.github.com>
2024-01-13 17:12:55 -0700

    Backend Changes For New Feature

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,11 @@
 // Imports
-import express from 'express';
-import { PORT, mongoDBURL } from './config.js';
-import mongoose from 'mongoose';
-import allowedUsersRoute from './routes/allowedUsersRoutes.js'
-import allowedPartnerRoutes from './routes/allowedPartnerRoutes.js'
-import cors from 'cors';
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import allowedUsersRoutes from "./routes/allowedUsersRoutes.js";
+import allowedPartnerRoutes from "./routes/allowedPartnerRoutes.js";
+import allowedRequestsRoutes from "./routes/allowedRequestsRoutes.js";
+import cors from "cors";
 
 // Base For Using Express
 const app = express();
@@ -16,14 +17,15 @@ app.use(express.json());
 app.use(cors());
 
 // Allowed Routes
-app.use('/users', allowedUsersRoute);
-app.use('/partners', allowedPartnerRoutes);
+app.use("/users", allowedUsersRoutes);
+app.use("/partners", allowedPartnerRoutes);
+app.use("/requests", allowedRequestsRoutes);
 
 //Database connection
 mongoose
   .connect(mongoDBURL)
   .then(() => {
-    console.log('Partnership Tracker connected to database');
+    console.log("Partnership Tracker connected to database");
     app.listen(PORT, () => {
       console.log(`Partnership Tracker is listening to port: ${PORT}`);
     });
---
+++ Backend/routes/allowedRequestsRoutes.js
@@ -0,0 +1,4 @@
+import express, { request, response } from "express";
+import { Request } from "../models/requestModel";
+
+const router = express.Router();
