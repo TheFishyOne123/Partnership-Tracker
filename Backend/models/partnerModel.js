9c7bff3cf3e6a614966849516cd2afe0ef22ee4c
TheFishyOne123 <81216766+TheFishyOne123@users.noreply.github.com>
2024-03-21 08:59:24 -0600

    Eslint Fixes

--- Backend/models/partnerModel.js
+++ Backend/models/partnerModel.js
@@ -3,42 +3,42 @@ import mongoose from 'mongoose'
 // Partner Model
 
 const partnersSchema = mongoose.Schema({
-    companyName: {
-        type: String,
-        required: true
-    },
-    position: {
-        type: String,
-        required: true
-    },
-    owner: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String,
-        required: true
-    },
-    pathway: {
-        type: String,
-        required: true
-    },
-    timeOfDay: {
-        type: String,
-        required: true
-    },
-    firstDayAvailable: {
-        type: String,
-        required: true
-    },
-    lastDayAvailable: {
-        type: String,
-        required: true
-    }
+  companyName: {
+    type: String,
+    required: true
+  },
+  position: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  pathway: {
+    type: String,
+    required: true
+  },
+  timeOfDay: {
+    type: String,
+    required: true
+  },
+  firstDayAvailable: {
+    type: String,
+    required: true
+  },
+  lastDayAvailable: {
+    type: String,
+    required: true
+  }
 })
 
 // Exporting For Use In Database
