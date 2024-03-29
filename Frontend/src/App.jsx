fc186d37f6fbb8723c027778cc371edaae2b7a28
TheFishyOne123 <81216766+TheFishyOne123@users.noreply.github.com>
2024-03-28 20:21:56 -0600

    ESlint Fixes
    Fixes For ESlint Standard

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import HomePage from "./pages/HomePage";
-import AdminPage from "./pages/AdminPage";
-import UsersAdminPage from "./pages/UsersAdminPage";
-import PartnersAdminPage from "./pages/PartnersAdminPage";
-import PartnerRequestsPage from "./pages/PartnerRequestsPage";
-import "./index.css";
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
+import AdminPage from './pages/AdminPage'
+import UsersAdminPage from './pages/UsersAdminPage'
+import PartnersAdminPage from './pages/PartnersAdminPage'
+import PartnerRequestsPage from './pages/PartnerRequestsPage'
+import './index.css'
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/user" element={<HomePage />} />
-      <Route path="/admin" element={<AdminPage />} />
-      <Route path="/admin/users" element={<UsersAdminPage />} />
-      <Route path="/admin/partners" element={<PartnersAdminPage />} />
-      <Route path="/admin/requests" element={<PartnerRequestsPage />} />
+      <Route path='/' element={<LoginPage />} />
+      <Route path='/user' element={<HomePage />} />
+      <Route path='/admin' element={<AdminPage />} />
+      <Route path='/admin/users' element={<UsersAdminPage />} />
+      <Route path='/admin/partners' element={<PartnersAdminPage />} />
+      <Route path='/admin/requests' element={<PartnerRequestsPage />} />
     </Routes>
-  );
-};
+  )
+}
 
-export default App;
+export default App
